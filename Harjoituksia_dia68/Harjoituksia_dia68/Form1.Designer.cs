f41c910db78cc6b78353706b369cdfd7fc6ae21a
Hexulez <henry.juhola@gmail.com>
2022-03-01 11:59:12 +0000

    koneen vaihto

---
+++ Harjoituksia_dia68/Harjoituksia_dia68/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Harjoituksia_dia68
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.teht1BT = new System.Windows.Forms.Button();
+            this.teht1LB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tehtävä 2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tehtävä 1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tehtävä 2";
+            // 
+            // teht1BT
+            // 
+            this.teht1BT.Location = new System.Drawing.Point(115, 19);
+            this.teht1BT.Name = "teht1BT";
+            this.teht1BT.Size = new System.Drawing.Size(75, 23);
+            this.teht1BT.TabIndex = 1;
+            this.teht1BT.Text = "Tehtävä 1";
+            this.teht1BT.UseVisualStyleBackColor = true;
+            this.teht1BT.Click += new System.EventHandler(this.teht1BT_Click);
+            // 
+            // teht1LB
+            // 
+            this.teht1LB.AutoSize = true;
+            this.teht1LB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teht1LB.Location = new System.Drawing.Point(12, 22);
+            this.teht1LB.Name = "teht1LB";
+            this.teht1LB.Size = new System.Drawing.Size(96, 20);
+            this.teht1LB.TabIndex = 0;
+            this.teht1LB.Text = "plussa lasku";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.teht1BT);
+            this.Controls.Add(this.teht1LB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Valikko";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button teht1BT;
+        private System.Windows.Forms.Label teht1LB;
+    }
+}
+
---
+++ Harjoituksia_dia68/Harjoituksia_dia68/tehtava1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Harjoituksia_dia68
+{
+    partial class teht1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SummaLB = new System.Windows.Forms.Label();
+            this.laskeBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna 2 lukua niin lasken ne yhteen";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(261, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "=";
+            // 
+            // SummaLB
+            // 
+            this.SummaLB.AutoSize = true;
+            this.SummaLB.Location = new System.Drawing.Point(291, 45);
+            this.SummaLB.Name = "SummaLB";
+            this.SummaLB.Size = new System.Drawing.Size(12, 13);
+            this.SummaLB.TabIndex = 0;
+            this.SummaLB.Text = "x";
+            this.SummaLB.Visible = false;
+            // 
+            // laskeBT
+            // 
+            this.laskeBT.Location = new System.Drawing.Point(32, 68);
+            this.laskeBT.Name = "laskeBT";
+            this.laskeBT.Size = new System.Drawing.Size(75, 23);
+            this.laskeBT.TabIndex = 2;
+            this.laskeBT.Text = "laske";
+            this.laskeBT.UseVisualStyleBackColor = true;
+            this.laskeBT.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // teht1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 113);
+            this.Controls.Add(this.laskeBT);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SummaLB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "teht1";
+            this.Text = "tehtava1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label SummaLB;
+        private System.Windows.Forms.Button laskeBT;
+    }
+}
