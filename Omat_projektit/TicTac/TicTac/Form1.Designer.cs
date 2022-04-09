fd8b823db83a2b368d4cf6179372683ebe67baed
Hexulez <henry.juhola@edu.keuda.fi>
2022-04-09 21:20:07 +0300

    tictactoe

---
+++ Omat_projektit/TicTac/TicTac/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace TicTac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.but1 = new System.Windows.Forms.Button();
+            this.but2 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.but5 = new System.Windows.Forms.Button();
+            this.but6 = new System.Windows.Forms.Button();
+            this.but7 = new System.Windows.Forms.Button();
+            this.but8 = new System.Windows.Forms.Button();
+            this.but9 = new System.Windows.Forms.Button();
+            this.restartBT = new System.Windows.Forms.Button();
+            this.selectCB = new System.Windows.Forms.ComboBox();
+            this.statusLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // but1
+            // 
+            this.but1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but1.Location = new System.Drawing.Point(110, 179);
+            this.but1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(143, 133);
+            this.but1.TabIndex = 1;
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // but2
+            // 
+            this.but2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but2.Location = new System.Drawing.Point(262, 179);
+            this.but2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(143, 133);
+            this.but2.TabIndex = 1;
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // but3
+            // 
+            this.but3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but3.Location = new System.Drawing.Point(414, 179);
+            this.but3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(143, 133);
+            this.but3.TabIndex = 1;
+            this.but3.UseVisualStyleBackColor = true;
+            this.but3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // but4
+            // 
+            this.but4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but4.Location = new System.Drawing.Point(110, 321);
+            this.but4.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(143, 133);
+            this.but4.TabIndex = 1;
+            this.but4.UseVisualStyleBackColor = true;
+            this.but4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // but5
+            // 
+            this.but5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but5.Location = new System.Drawing.Point(262, 321);
+            this.but5.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.but5.Name = "but5";
+            this.but5.Size = new System.Drawing.Size(143, 133);
+            this.but5.TabIndex = 1;
+            this.but5.UseVisualStyleBackColor = true;
+            this.but5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // but6
+            // 
+            this.but6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but6.Location = new System.Drawing.Point(414, 321);
+            this.but6.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.but6.Name = "but6";
+            this.but6.Size = new System.Drawing.Size(143, 133);
+            this.but6.TabIndex = 1;
+            this.but6.UseVisualStyleBackColor = true;
+            this.but6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // but7
+            // 
+            this.but7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but7.Location = new System.Drawing.Point(110, 463);
+            this.but7.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.but7.Name = "but7";
+            this.but7.Size = new System.Drawing.Size(143, 133);
+            this.but7.TabIndex = 1;
+            this.but7.UseVisualStyleBackColor = true;
+            this.but7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // but8
+            // 
+            this.but8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but8.Location = new System.Drawing.Point(262, 463);
+            this.but8.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.but8.Name = "but8";
+            this.but8.Size = new System.Drawing.Size(143, 133);
+            this.but8.TabIndex = 1;
+            this.but8.UseVisualStyleBackColor = true;
+            this.but8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // but9
+            // 
+            this.but9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but9.Location = new System.Drawing.Point(414, 463);
+            this.but9.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.but9.Name = "but9";
+            this.but9.Size = new System.Drawing.Size(143, 133);
+            this.but9.TabIndex = 1;
+            this.but9.UseVisualStyleBackColor = true;
+            this.but9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // restartBT
+            // 
+            this.restartBT.Location = new System.Drawing.Point(664, 508);
+            this.restartBT.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.restartBT.Name = "restartBT";
+            this.restartBT.Size = new System.Drawing.Size(182, 88);
+            this.restartBT.TabIndex = 2;
+            this.restartBT.Text = "Restart";
+            this.restartBT.UseVisualStyleBackColor = true;
+            // 
+            // selectCB
+            // 
+            this.selectCB.FormattingEnabled = true;
+            this.selectCB.Location = new System.Drawing.Point(657, 369);
+            this.selectCB.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.selectCB.Name = "selectCB";
+            this.selectCB.Size = new System.Drawing.Size(186, 37);
+            this.selectCB.TabIndex = 3;
+            // 
+            // statusLB
+            // 
+            this.statusLB.AutoSize = true;
+            this.statusLB.Location = new System.Drawing.Point(29, 50);
+            this.statusLB.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.statusLB.Name = "statusLB";
+            this.statusLB.Size = new System.Drawing.Size(79, 29);
+            this.statusLB.TabIndex = 4;
+            this.statusLB.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 665);
+            this.Controls.Add(this.statusLB);
+            this.Controls.Add(this.selectCB);
+            this.Controls.Add(this.restartBT);
+            this.Controls.Add(this.but9);
+            this.Controls.Add(this.but6);
+            this.Controls.Add(this.but3);
+            this.Controls.Add(this.but8);
+            this.Controls.Add(this.but5);
+            this.Controls.Add(this.but2);
+            this.Controls.Add(this.but7);
+            this.Controls.Add(this.but4);
+            this.Controls.Add(this.but1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Button but5;
+        private System.Windows.Forms.Button but6;
+        private System.Windows.Forms.Button but7;
+        private System.Windows.Forms.Button but8;
+        private System.Windows.Forms.Button but9;
+        private System.Windows.Forms.Button restartBT;
+        private System.Windows.Forms.ComboBox selectCB;
+        private System.Windows.Forms.Label statusLB;
+    }
+}
+
---
+++ Omat_projektit/TicTac/TicTac/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTac
+{
+    public partial class Form1 : Form
+    {
+        string turn;
+        int counter;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            counter++;
+            ((Button)sender).Text = turn;
+            if (turn == "X")
+            {
+                turn = "O";
+            }
+            else
+            {
+                turn = "X";
+            }
+        }
+
+         private void Restart()
+         {
+            but1.
+
+         }
+        
+
+        
+    }
+}
