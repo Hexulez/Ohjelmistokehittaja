9126d75f6ad5e0920aef05eedbbf4c4159df9cef
Hexulez <henry.juhola@edu.keuda.fi>
2022-05-18 16:36:41 +0300

    inventory alpha

---
+++ temp/PicBox_Test-Chamber/PicBox_Test-Chamber/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace PicBox_Test_Chamber
+{
+    partial class carGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.myCar = new System.Windows.Forms.PictureBox();
+            this.Enemy = new System.Windows.Forms.PictureBox();
+            this.TicTocTimer = new System.Windows.Forms.Timer(this.components);
+            this.enemyTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.myCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myCar
+            // 
+            this.myCar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.myCar.Location = new System.Drawing.Point(12, 197);
+            this.myCar.Name = "myCar";
+            this.myCar.Size = new System.Drawing.Size(76, 47);
+            this.myCar.TabIndex = 0;
+            this.myCar.TabStop = false;
+            // 
+            // Enemy
+            // 
+            this.Enemy.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Enemy.Location = new System.Drawing.Point(712, 197);
+            this.Enemy.Name = "Enemy";
+            this.Enemy.Size = new System.Drawing.Size(76, 47);
+            this.Enemy.TabIndex = 0;
+            this.Enemy.TabStop = false;
+            // 
+            // TicTocTimer
+            // 
+            this.TicTocTimer.Enabled = true;
+            this.TicTocTimer.Tick += new System.EventHandler(this.TicTocTimer_Tick);
+            // 
+            // enemyTimer
+            // 
+            this.enemyTimer.Enabled = true;
+            this.enemyTimer.Tick += new System.EventHandler(this.enemyTimer_Tick);
+            // 
+            // carGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Enemy);
+            this.Controls.Add(this.myCar);
+            this.Name = "carGame";
+            this.Text = "carGame";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.carGame_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.carGame_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.myCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox myCar;
+        private System.Windows.Forms.PictureBox Enemy;
+        private System.Windows.Forms.Timer TicTocTimer;
+        private System.Windows.Forms.Timer enemyTimer;
+    }
+}
+
---
+++ temp/PicBox_Test-Chamber/PicBox_Test-Chamber/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+ 
+
+namespace PicBox_Test_Chamber
+{
+    public partial class carGame : Form
+    {
+        public carGame()
+        {
+            InitializeComponent();
+        }
+        bool goLeft, goRight, goUp = false, goDown = false;
+        int x,y, playerMove = 10;
+        Random random = new Random();
+
+        private void enemyTimer_Tick(object sender, EventArgs e)
+        {
+            MakeEnemy();
+        }
+
+        List<PictureBox> enemyList = new List<PictureBox>();
+
+
+        //player movement
+        private void TicTocTimer_Tick(object sender, EventArgs e)
+        {
+            if (goUp == true)
+            {
+                myCar.Top -= playerMove;
+            }
+            else if (goDown == true)
+            {
+                myCar.Top += playerMove;
+            }
+
+
+        }
+        
+
+       
+
+        private void carGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            else if (e.KeyCode== Keys.Left)
+            {
+                goLeft = true;
+            }
+            else if (e.KeyCode== Keys.Right) 
+            { 
+                goRight = true; 
+            }
+
+        }
+
+        private void carGame_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            else if (e.KeyCode== Keys.Left)
+            {
+                goLeft = false;
+            }
+            else if (e.KeyCode== Keys.Right)
+            {
+                goRight = false;
+            }
+        }
+        //player movement end
+        private void MakeEnemy()
+        {
+            PictureBox enemy = new PictureBox();
+            enemy.Width =   50;
+            enemy.Height = 50;
+            enemy.BackColor = Color.Blue;
+
+            x = this.ClientSize.Width - enemy.Width;
+            y = random.Next(10, this.ClientSize.Height - enemy.Height);
+            
+            enemy.Location = new Point(x, y);
+            enemyList.Add(enemy);
+            this.Controls.Add(enemy);
+
+
+        }
+
+
+    }
+}
