99490bf8f6906fdf351d41fe62ecffc751149e38
Hexulez <henry.juhola@edu.keuda.fi>
2022-05-18 10:41:37 +0300

    CarGame rautalanka

---
+++ temp/Canvas tester/Canvas tester/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Canvas_tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int paikka = 0;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PressButton(object sender, KeyPressEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+
+            Brush snakeColour; //luo muuttujan millä määritellään myöhemmin pallojen väri
+
+            snakeColour = Brushes.Black;
+            
+
+            canvas.FillEllipse(snakeColour, new Rectangle
+            (
+            1 * Settings.Width,
+            Snake[i].Y * Settings.Height,
+            Settings.Width, Settings.Height
+            ));
+
+
+            
+        }
+    }
+}
