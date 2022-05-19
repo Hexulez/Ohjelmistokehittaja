ea1e328a70e22ebdd21409f529f7037f2d0fd66a
Hexulez <henry.juhola@edu.keuda.fi>
2022-05-19 15:23:52 +0300

    Shooting game

---
+++ Omat_projektit/Shooting_game/Shooting_game/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Shooting_game
+{
+    partial class shootingGameFM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PlayerPB = new System.Windows.Forms.PictureBox();
+            this.AimingPB = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AimingPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayerPB
+            // 
+            this.PlayerPB.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PlayerPB.Location = new System.Drawing.Point(389, 291);
+            this.PlayerPB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.PlayerPB.Name = "PlayerPB";
+            this.PlayerPB.Size = new System.Drawing.Size(33, 30);
+            this.PlayerPB.TabIndex = 0;
+            this.PlayerPB.TabStop = false;
+            // 
+            // AimingPB
+            // 
+            this.AimingPB.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.AimingPB.Location = new System.Drawing.Point(137, 158);
+            this.AimingPB.Name = "AimingPB";
+            this.AimingPB.Size = new System.Drawing.Size(27, 27);
+            this.AimingPB.TabIndex = 1;
+            this.AimingPB.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // shootingGameFM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 659);
+            this.Controls.Add(this.AimingPB);
+            this.Controls.Add(this.PlayerPB);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "shootingGameFM";
+            this.Text = "Shooting_game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.shootingGameFM_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.shootingGameFM_KeyUp);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.shootingGameFM_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AimingPB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PlayerPB;
+        private System.Windows.Forms.PictureBox AimingPB;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Omat_projektit/Shooting_game/Shooting_game/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shooting_game
+{
+    public partial class shootingGameFM : Form
+    {
+        public shootingGameFM()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        List<PictureBox> zombies = new List<PictureBox>();
+        List <PictureBox> ammo = new List<PictureBox>();
+        bool goLeft, goRight, goDown, goUp;
+        int playerMoving = 10;
+
+        private void Moving()
+        {
+            if (goLeft) PlayerPB.Left -= playerMoving;
+            if (goRight) PlayerPB.Left += playerMoving;
+            if (goUp) PlayerPB.Top -= playerMoving;
+            if (goDown) PlayerPB.Top += playerMoving; 
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            Moving();
+        }
+
+        private void shootingGameFM_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) { goLeft = true; }
+            if (e.KeyCode == Keys.Right) { goRight = true; }
+            if (e.KeyCode == Keys.Up) { goUp = true; }
+            if (e.KeyCode == Keys.Down) { goDown = true; }
+        }
+
+        private void shootingGameFM_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) { goLeft = false; }
+            if (e.KeyCode == Keys.Right) { goRight = false; }
+            if (e.KeyCode == Keys.Up) { goUp = false; }
+            if (e.KeyCode ==Keys.Down) { goDown = false; }
+        }
+
+        private void shootingGameFM_MouseMove(object sender, MouseEventArgs e)
+        {
+            AimingPB.Location = e.Location;
+        }
+
+        private void Shooting()
+        {
+            int startX = PlayerPB.Location.X;
+            int startY = PlayerPB.Location.Y;
+            int endX = AimingPB.Location.X;
+            int endY = AimingPB.Location.Y;
+            int flyX = startX - endX;
+            int flyY = startY - endY;
+            if (flyX < flyY)
+            {
+                //float littleMove = (flyX - flyY) / (flyY - flyX);
+                float littleMove = flyX / flyY;
+            }
+
+        }
+    }
+}
